d4957df32624d81b877a06f1c0641b173899f01d
Pure Bird <45554491+lijianwei-inChina@users.noreply.github.com>
2021-09-24 20:54:49 +0800

    增加MainPage的自适应，增加js的备份

---
+++ js_backup/手机视图自适应.js
@@ -0,0 +1,10 @@
+$(function() {
+	if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		$("span").filter(".normal-title").addClass("small-title").removeClass("normal-title"); // 页面div的标题
+		$("span").filter(".normal-spirit-size").addClass("small-spirit-size").removeClass("normal-spirit-size"); // 班旗班徽标题
+		$("span").filter(".normal-text").addClass("small-text").removeClass("normal-text"); // text自适应
+		$("span").filter(".normal-text-br").addClass("small-text-br").removeClass("normal-text-br"); // br自适应
+	} else {
+		
+	}
+});
