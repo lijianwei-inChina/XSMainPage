a19524457c2eb0ab42eff13040870dfb68a5b2f9
Pure Bird <45554491+lijianwei-inChina@users.noreply.github.com>
2021-11-27 21:28:09 +0800

    backup

---
+++ MainPage/V2.0/js/font_size_selfsuit.js
@@ -0,0 +1,16 @@
+function Size() {
+	//根据屏幕大小切换字体
+	// 动态设置html标签的字体大小从而定了rem的大小
+	var Ohtml = document.documentElement;
+	var screenWidth = Ohtml.clientWidth;
+	if (screenWidth <= 320) {
+		Ohtml.style.fontSize = '12.5px';
+	} else if (screenWidth >= 768) {
+		Ohtml.style.fontSize = '30px';
+	} else {
+		Ohtml.style.fontSize = screenWidth / (25.6) + 'px';
+	}
+}
+$(function() {
+	Size();
+})
---
+++ MainPage/V2.0/js/page_listener.js
@@ -0,0 +1,20 @@
+/**
+ * see: font_size_selfsuit.js
+ */
+function Size() {
+	//根据屏幕大小切换字体
+	// 动态设置html标签的字体大小从而定了rem的大小
+	var Ohtml = document.documentElement;
+	var screenWidth = Ohtml.clientWidth;
+	if (screenWidth <= 320) {
+		Ohtml.style.fontSize = '6.25px';
+	} else if (screenWidth >= 768) {
+		Ohtml.style.fontSize = '15px';
+	} else {
+		Ohtml.style.fontSize = screenWidth / (51.2) + 'px';
+	}
+}
+$(window).resize(function() {
+ 	//监听窗口大小变化
+ 	Size();
+ });
