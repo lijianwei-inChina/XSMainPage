ae4340e6043dc3afb8b318c0f2f3783a107d5580
Pure Bird <45554491+lijianwei-inChina@users.noreply.github.com>
2022-05-28 21:33:41 +0800

    Update XiaShuoEnglish; Update Mainpage

---
+++ MainPage/栏目/“夏说英语”转载/resize.js
@@ -0,0 +1,8 @@
+{
+	function resize() {
+		document.querySelector("html").style.fontSize = document.querySelector("html").getBoundingClientRect().width /
+			76.1 + "px";
+	}
+	resize();
+	window.addEventListener("resize", resize);
+}
